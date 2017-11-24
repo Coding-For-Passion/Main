9fe8d02116fe476ed6f036daa502dd6be4401faa
Alexander4334 <33609792+Alexander4334@users.noreply.github.com>
2017-11-24 11:21:27 -0700

    Create js.js

---
+++ Overwatch-Test/js.js
@@ -0,0 +1,6 @@
+alert("This is one type of popup")
+confirm("This is another type of popup")
+var quiz = prompt("This is the las type of :")
+if (quiz == "popup") {
+  alert("Correct, you are getting the hang of this")
+}
