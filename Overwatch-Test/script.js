16c2cf307ade74454d135f20ffaef594364e60a0
Alexander4334 <33609792+Alexander4334@users.noreply.github.com>
2017-11-24 12:51:52 -0700

    Delete script.js

--- Overwatch-Test/script.js
+++
@@ -1,4 +0,0 @@
-var confirm = confirm("This is site may be MLG. Are you sure you want to continue?");
-if (!confirm) {
-  location.href = 'https://www.google.ca/?gfe_rd=cr&dcr=0&ei=QnIYWrD2PMze8Afu5IDoDA';
-}
