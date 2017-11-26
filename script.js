859672685e9b70946d9c2053e8eff481ebdad201
Alexander4334 <33609792+Alexander4334@users.noreply.github.com>
2017-11-26 13:27:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ var project = 0;
 function addProject (x) {
 project = project + x;
 }
-alert("Welcome to our AI projects website. There are currently: " + project + " projects on right now.");
+var word = " projects";
+var word2 = "are";
+if (project == 1) } {
+  word = " project"
+  word2 = "is"
+}
+alert("Welcome to our AI projects website. There " + word2 + " currently " + project + word + " on right now.");
