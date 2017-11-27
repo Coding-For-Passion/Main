0bdef554ff0c2a3a93404e01aa1a32c55869cc68
Alexander4334 <33609792+Alexander4334@users.noreply.github.com>
2017-11-26 18:47:48 -0700

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,23 +1,23 @@
 // alert("Sign-In is currently disabled on the main page, A Sign-in popup will appear shortly!");
 var f = confirm("Are you sure you want to make changes to this website (please note changes will not be immediate)");
-if (f = false) {
-        Location.replace("index.html");
+if (f == false) {
+	Location.replace("index.html");
 }
 else {
-while(true) {
-var subject = prompt("What is the subject you would like to fix?");
-var body = prompt("What exactly would you like to fix?");
-if {
-alert("Nothing Submitted")
-}
-else {
-break;
-}        
-subject = "Fix: " + subject;
-body = "A user has requested to fix: " + body;
-
-var popupBlockerChecker = {
-        check: function(popup_window) {
+	while (true) {
+		var subject = prompt("What is the subject you would like to fix?");
+        var body = prompt("What exactly would you like to fix?");
+		if {
+			alert("Nothing Submitted");
+		}
+		else {
+			break;
+        }
+        }
+		subject = "Fix: " + subject;
+		body = "A user has requested to fix: " + body;
+		var popupBlockerChecker = {
+			check: function(popup_window) {
             var _scope = this;
             if (popup_window) {
                 if(/chrome/.test(navigator.userAgent.toLowerCase())){
@@ -42,5 +42,4 @@ var popupBlockerChecker = {
     };
 var popup = window.open("mailto:wesbob12@gmail.com" + "?subject=" + subject + "&body=" + body);
 popupBlockerChecker.check(popup);
-}
 }
