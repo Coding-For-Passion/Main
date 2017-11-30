b810f4c5550c9c05672fd83f39b8d64079ac094b
Riley <31376849+Rywee@users.noreply.github.com>
2017-11-30 10:51:53 -0700

    Create nim.js

---
+++ nim.js
@@ -0,0 +1,18 @@
+var nims = 21;
+function play() {
+  nims -= document.getElementById("select").value; 
+  document.getElementById("nims").innerHTML = "Nims Remaining: " + nims;
+  if (nims == 0) {document.getElementById("nims").innerHTML = "You Win!!"}
+  setTimeout(AI, 2000);
+}
+
+function AI() {
+  if (nims % 4 == 0) {
+    nims -= Math.floor(Math.random() * 3) + 1;
+  } else {
+    nims -= (nims % 4); 
+  }
+  document.getElementById("nims").innerHTML = "Nims Remaining: " + nims;
+  if (nims == 0) {document.getElementById("nims").innerHTML = "You Lose!!"} else 
+  if (nims < 0) {document.getElementById("nims").innerHTML = "You Win!!"}
+}
