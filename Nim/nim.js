43d6c11e484cb1c3eb60fdc471d9d4a3aba404c3
Alexander4334 <33609792+Alexander4334@users.noreply.github.com>
2017-12-09 16:24:40 -0700

    Update nim.js

--- Nim/nim.js
+++ Nim/nim.js
@@ -1,16 +1,18 @@
 var nims = 21;
 var yourTurn = true;
+function nameThis() {
+  document.getElementById("nims").innerHTML = "Nims Remaining: " + nims;
+}
 var set = prompt("How many nim would you like to start with? ");
 if (set != null) {
   nims = set;
-  setTimeout("document.getElementById("nims").innerHTML = "Nims Remaining: " + nims;",100)
+  
+  setTimeout(nameThis,100);
 }
 var firstTurn = confirm("Would you like to go first?");
-if (firstTurn == true) {
-}
+
 if (firstTurn == false) {
   setTimeout(AI, 2000);
-  yourTurn = true;
 }
 function play() {
   if (nims == 0) {
