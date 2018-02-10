8e8135d355fa25351ffe167567af0dc295edc224
Alexander4334 <33609792+Alexander4334@users.noreply.github.com>
2018-02-10 10:27:46 -0700

    Update nim.js

--- Nim/nim.js
+++ Nim/nim.js
@@ -9,7 +9,7 @@ var retry = false;
 var userChoice;
 var userChoiceLength;
 var ifNumber;
-var EXPERIMENTAL_MODE = true;
+var EXPERIMENTAL_MODE = false;
 var modeMessage = "Hi, I am Jeeves, your trusty AI. I would be delighted to play a game of Nim";
 //var nimDisplay;
 //function display(nims) {
@@ -32,7 +32,7 @@ function userChoosing() {
   userChoice = prompt("How many nim would you like to start with?(2 digit number)");
   firstTurn = confirm("Would you like to go first?");
   userChoiceLength = userChoice.length;
-  ifNumber = userChoice.isNaN;
+  ifNumber = isNaN(userChoice);
   if (userChoice == null || ifNumber) {
     retry = confirm("ERROR: NaN or no Input\nNot a number, retry?" );
     if (retry) {
@@ -51,16 +51,30 @@ function userChoosing() {
     }
   }
 }
-checkMode(EXPERIMENTAL_MODE);
-alert(modeMessage);  
-alert("To play Nim, you choose a number of 'Nims', and then you choose whether to go first or second. You and the AI take turns removing blocks — each player may remove one to three nims, whichever player takes the last Nim wins.");
-userChoosing();
-if (firstTurn == false) {
-  setTimeout(AI, 2000);
+function AI() {
+  document.getElementById("move").innerHTML = "AI Move";
+  if (nims % 4 == 0) {
+    nims -= Math.floor(Math.random() * 3) + 1;
+  } else {
+    nims -= (nims % 4); 
+  }
+  if (nims > 0) {
+  document.getElementById("nims").innerHTML = "Nims Remaining: " + nims;}
+  else if (nims == 0) {
+    document.getElementById("nims").innerHTML = "You Lose!!";
+    setTimeout(playAgain,500);
+  } else if (nims < 0) {
+    document.getElementById("nims").innerHTML = "You Win!!";
+   setTimeout(playAgain,500);
+  }
+  document.getElementById("move").innerHTML = "Your Move";
+//  displayReset();
+//  display(nims);
+  yourTurn = true;
 }
-function play() {
-  if (nims == 0) {
-    document.getElementById("nims").innerHTML = "You Win!!"
+  function play() {
+    if (nims == 0) {
+    document.getElementById("nims").innerHTML = "You Win!!";
   } else {
     if (yourTurn == true) {
       nims -= document.getElementById("select").value; 
@@ -74,35 +88,20 @@ function play() {
       alert("It isn't your turn yet!!"); 
     }
   }
-}
-function playAgain() {
-var con = confirm("Would you like to play again?")
+  }
+  function playAgain() {
+  var con = confirm("Would you like to play again?");
     if (con == false) {
-    location.replace('../Homepage/homepage.html');
+      location.replace('../Homepage/homepage.html');
     }
     if (con == true) {
-    location.replace('nim.html');
+      location.replace('nim.html');
     }
 }
-function AI() {
-  document.getElementById("move").innerHTML = "AI Move";
-  if (nims % 4 == 0) {
-    nims -= Math.floor(Math.random() * 3) + 1;
-  } else {
-    nims -= (nims % 4); 
+  checkMode(EXPERIMENTAL_MODE);
+  alert(modeMessage);  
+  alert("To play Nim, you choose a number of 'Nims', and then you choose whether to go first or second. You and the AI take turns removing blocks — each player may remove one to three nims, whichever player takes the last Nim wins.");
+  userChoosing();
+  if (firstTurn == false) {
+    setTimeout(AI, 2000);
   }
-  if (nims > 0) {
-  document.getElementById("nims").innerHTML = "Nims Remaining: " + nims;}
-  else if (nims == 0) {
-    document.getElementById("nims").innerHTML = "You Lose!!"
-    setTimeout(playAgain,500);
-  } else if (nims < 0) {
-    document.getElementById("nims").innerHTML = "You Win!!"
-   setTimeout(playAgain,500);
-  }
-  document.getElementById("move").innerHTML = "Your Move";
-//  displayReset();
-//  display(nims);
-  yourTurn = true;
-  
-}
